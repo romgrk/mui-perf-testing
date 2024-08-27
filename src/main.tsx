60418c0a7642fa3beeeb9c593b6fdb601430050a
Rom Grk <romgrk.cc@gmail.com>
2024-08-27 12:03:40 -0400

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-import '@pigment-css/react/styles.css';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { pigment } from '@pigment-css/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: /^@emotion\/(.+)$/,
+        // @ts-ignore
+        replacement: path.join(import.meta.dirname, '../emotion/packages/$1')
+      },
+    ],
+  },
   build: {
     minify: false,
     sourcemap: true,
@@ -13,11 +22,6 @@ export default defineConfig({
     },
   },
   plugins: [
-    pigment({
-      theme: {
-        color: 'red',
-      }
-    }),
     react(),
   ],
 })
